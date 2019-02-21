33a9f0614385ebff4b57ca4576c4bf8fc7eca046
fcsmandrei <leonchikovandrei@gmail.com>
2019-02-22 00:33:09 +0300

    Count triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+	let count = 0;
+	let first, second, third;
+	const len = preferences.length;
+	for(let i=0; i<len ; i++){
+		third = preferences[i];
+		second = preferences[third - 1];
+		first = preferences[second -1];
+
+		if (first == i+1 && second > i+1 && third > i+1){
+            count++;
+		}	
+	};
+
+	return count;
+}
